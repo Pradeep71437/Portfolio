1b5e50e870a6e8693cc8adee50d69d1748dcd545
Pradeep <pradee.sinivas@gmail.com>
2024-10-29 22:32:14 +0530

    dc

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import {motion} from 'framer-motion'
 import { styles } from '../styles'
-import profile from '../assets/pradeep.png'
-// import { ComputersCanvas } from './canvas'
+import { ComputersCanvas } from './canvas'
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto '>
-        <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start justify-center `}>
+    <section className='relative w-full h-screen mx-auto'>
+        <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start justify-center gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           {/* <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' /> */}
@@ -18,32 +17,10 @@ const Hero = () => {
           <p className={`${styles.heroSubText} text-center mt-2 text-white-100`}>
             Full Stack Web Developer
           </p>
-          <div className='flex justify-center items-center '>
-          <div className="relative inline-block">
-  {/* Image with SVG filter for outline */}
-  <svg className="absolute -z-10">
-    <filter id="outline">
-      <feMorphology operator="dilate" radius="5" in="SourceAlpha" result="outline" />
-      <feFlood floodColor="purple" />
-      <feComposite in2="outline" operator="in" />
-      <feMerge>
-        <feMergeNode />
-        <feMergeNode in="SourceGraphic" />
-      </feMerge>
-    </filter>
-  </svg>
-  <img
-    src={profile}
-    className="lg:w-[800px] w-[900px] h-[400px] lg:h-[600px] object-cover object-top"
-    alt=""
-    style={{ filter: 'url(#outline)' }} // Applying the outline filter
-  />
-</div>
-          </div>
         </div>
 
         </div>
-        {/* <ComputersCanvas /> */}
+        <ComputersCanvas />
     </section>
   )
 }
